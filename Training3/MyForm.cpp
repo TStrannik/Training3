9219faf40399eba336f79980c80390c2d5091af5
aleksandr.galiew@gmail.com <aleksandr.galiew@gmail.com>
2024-08-09 13:21:27 +0300

    ver 0.0.0

--- Training3/MyForm.cpp
+++ Training3/MyForm.cpp
@@ -1,2 +1,40 @@
 #include "MyForm.h"
+#include <Windows.h>
+#include <iostream>
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+
+
+void APPLICATION_START() {
+	system("color 70");
+	std::cout << "\n APPLICATION START\n\n";
+	std::cout << "\tStatistic:\n\n";
+
+	
+
+	std::cout << "\n\n";
+}
+void APPLICATION_FORM() {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	Training3::MyForm form;
+	Application::Run(% form);
+}
+void APPLICATION_END() {
+	std::cout << "\nAPPLICATION END\n\n";
+	system("pause");
+}
+
+
+
+// ver 0.1.0
+int main(array<String^>^ args) {
+	APPLICATION_START();
+	APPLICATION_FORM();
+	APPLICATION_END();
+	return 0;
+}
